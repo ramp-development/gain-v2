10fe71872ee8bea4d9d2d44548552fd99d67c278
g4knr <contact@g4knr.co.uk>
2025-09-25 10:04:20 +0100

    Changed name to getEnvironment to be clearer

--- src/utils/environment.ts
+++
@@ -1,6 +0,0 @@
-import type { Environment } from 'src/types';
-
-export const environment = (): Environment => {
-  const { host } = window.location;
-  return host.includes('webflow.io') ? 'staging' : 'production';
-};
