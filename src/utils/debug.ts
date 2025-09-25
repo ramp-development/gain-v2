4ecb62cbb6d88fceebf8ccc90f2d1a57680f0bcf
g4knr <contact@g4knr.co.uk>
2025-09-25 13:28:45 +0100

    Made data optional

--- src/utils/debug.ts
+++ src/utils/debug.ts
@@ -1,7 +1,7 @@
 /* eslint-disable no-console */
 import { App } from '$app';
 
-export const debug = (type: 'log' | 'warn' | 'error', message: string, data: unknown) => {
+export const debug = (type: 'log' | 'warn' | 'error', message: string, data?: unknown) => {
   const app = App.getInstance();
   const { environment } = app;
   if (environment !== 'staging') return;
