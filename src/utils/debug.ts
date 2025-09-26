417da28e58eae71b04bfad84d0f99dc2fcfbd247
g4knr <contact@g4knr.co.uk>
2025-09-26 14:48:46 +0100

    Updated type

--- src/utils/debug.ts
+++ src/utils/debug.ts
@@ -1,7 +1,8 @@
 /* eslint-disable no-console */
 import { App } from '$app';
 
-export const debug = (type: 'log' | 'warn' | 'error', message: string, data?: unknown) => {
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export const debug = (type: 'log' | 'warn' | 'error', message: any, data?: any) => {
   const app = App.getInstance();
   const { environment } = app;
   if (environment !== 'staging') return;
