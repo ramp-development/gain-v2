842754093f0d407a58c7471cb5b69fb20aa31726
g4knr <contact@g4knr.co.uk>
2025-09-26 15:08:39 +0100

    Animated button

--- src/animations/timelines/aiTeamHero.ts
+++ src/animations/timelines/aiTeamHero.ts
@@ -1,4 +1,5 @@
 import type { TimelineCreator } from '$types';
+import { queryElement } from '$utils/queryElement';
 import { queryElements } from '$utils/queryElements';
 
 export const aiTeamHeroTimeline: TimelineCreator = (
@@ -10,6 +11,7 @@ export const aiTeamHeroTimeline: TimelineCreator = (
   });
 
   // Find elements to animate
+  const backBtn = queryElement('[data-navigate]');
   const profiles = queryElements('[data-profile]', element);
   const ditlItems = queryElements('.ditl_item');
 
@@ -32,6 +34,10 @@ export const aiTeamHeroTimeline: TimelineCreator = (
 
   if (ditlItems.length) {
     tl.fromTo(ditlItems, { opacity: 0, y: '2rem' }, { opacity: 1, y: 0, stagger: 0.1 }, '-=50%');
+  }
+
+  if (backBtn) {
+    tl.from(backBtn, { opacity: 0, x: '0.5rem' }, '<0.5');
   }
 
   return tl;
