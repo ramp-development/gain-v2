5dc08f25dd7c85c5d7ac35a7d5ed8474e93ef0f7
g4knr <contact@g4knr.co.uk>
2025-10-10 09:53:59 +0100

    Updated duration and stagger

--- src/animations/timelines/cardFlip.ts
+++ src/animations/timelines/cardFlip.ts
@@ -1,12 +1,22 @@
 import { Thresholds } from 'src/types/thresholds';
 
+import { attrs } from '$config/constants';
+
 // import { containerThreshold } from '$utils/containerThreshold';
 import { BaseAnimation } from './base/baseAnimation';
 
 export class CardFlipTimeline extends BaseAnimation {
+  protected duration: number;
+
+  constructor(element: HTMLElement) {
+    super(element);
+
+    this.duration = 1;
+  }
+
   protected createTimeline(): void {
     // Find elements to animate
-    const cards = this.queryElements('[data-element="card"]');
+    const cards = this.queryElements(`[${attrs.elements}="card"]`);
     if (!cards.length) return;
 
     const numberOfCards = cards.length;
@@ -27,32 +37,37 @@ export class CardFlipTimeline extends BaseAnimation {
     this.element.observeContainer(`(width < ${Thresholds.medium}rem)`, (match) => {
       if (match) {
         this.timeline.set(cards, {
-          x: (index) => `${index}rem`,
+          x: (index) => `${index * 0.5}rem`,
           yPercent: (index) => index * -100,
         });
         this.timeline.to(cards, {
           x: 0,
           opacity: 1,
           yPercent: 0,
-          duration: 1,
+          duration: this.duration,
           ease: 'back.inOut',
-          stagger: 0.1,
+          stagger: 0.05,
         });
       } else {
         this.timeline.set(leftCards, { xPercent: 100 });
         this.timeline.set(rightCards, { xPercent: -100 });
         this.timeline.set(cards, { y: (index) => `${index}rem` });
 
+        this.timeline.addLabel('cardStart');
+        this.timeline.to(cards, {
+          y: gsap.utils.random(-8, 8),
+          duration: this.duration / 2,
+        });
         this.timeline.to(cards, {
           y: 0,
-          duration: 0.5,
+          duration: this.duration / 2,
           ease: 'back.inOut',
-          stagger: 0.1,
+          stagger: 0.05,
         });
         this.timeline.to(
           cards,
-          { xPercent: 0, opacity: 1, duration: 0.5, ease: 'back.inOut', stagger: 0.1 },
-          '<0.2'
+          { xPercent: 0, opacity: 1, duration: this.duration, ease: 'back.inOut', stagger: 0.05 },
+          'cardStart+=0.1'
         );
       }
     });
