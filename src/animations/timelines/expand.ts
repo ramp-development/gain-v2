f58301479bcff27b231f6a596e1f95354020a234
g4knr <contact@g4knr.co.uk>
2025-10-10 09:53:44 +0100

    Added expand timeline

---
+++ src/animations/timelines/expand.ts
@@ -0,0 +1,23 @@
+import { attrs } from '$config/constants';
+
+import { BaseAnimation } from './base/baseAnimation';
+
+export class ExpandTimeline extends BaseAnimation {
+  protected createTimeline(): void {
+    const wrap = this.queryElement(`[${attrs.elements}="wrap"]`);
+    if (!wrap) return;
+
+    // Build animation sequence
+    this.timeline.to(wrap, { scale: () => window.innerWidth / this.element.offsetWidth });
+    this.timeline.to(wrap, { scale: 1 }, '>2');
+  }
+
+  protected getScrollTriggerConfig(): ScrollTrigger.Vars {
+    return {
+      trigger: this.element,
+      start: 'top bottom',
+      end: 'bottom top',
+      scrub: 1,
+    };
+  }
+}
