864e90abeb0f8f6ff3a7aa0c3fa57e0f5b93823d
g4knr <contact@g4knr.co.uk>
2025-10-10 09:52:18 +0100

    Doubled panelScale margin

--- src/animations/utils/panelScale.ts
+++ src/animations/utils/panelScale.ts
@@ -1,6 +1,7 @@
 import { getPanelMargin } from '$utils/getPanelMargin';
 
 export const panelScale = () => {
-  const scale = (window.innerWidth - (getPanelMargin() || 0)) / window.innerWidth;
+  const margin = getPanelMargin() || 0;
+  const scale = (window.innerWidth - margin * 2) / window.innerWidth;
   return scale;
 };
