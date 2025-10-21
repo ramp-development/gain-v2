69bcefb0c11a8978f6de5482d05b4dd0858cf961
g4knr <contact@g4knr.co.uk>
2025-10-21 12:32:44 +0100

    Only apply the scale to larger devices

--- src/animations/utils/panelScale.ts
+++ src/animations/utils/panelScale.ts
@@ -1,6 +1,12 @@
+import { Thresholds } from 'src/types/thresholds';
+
+import { containerThreshold } from '$utils/containerThreshold';
 import { getPanelMargin } from '$utils/getPanelMargin';
 
 export const panelScale = () => {
+  const isAboveLarge = containerThreshold(document.body, Thresholds.large, 'above');
+  if (!isAboveLarge) return 1;
+
   const margin = getPanelMargin() || 0;
   const scale = (window.innerWidth - margin * 2) / window.innerWidth;
   return scale;
